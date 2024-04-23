6557e6c38915a76cc3376cb3942423fbaa78a0a2
DipuTut <mddipu@gmail.com>
2024-04-23 10:56:49 +0200

    Refactoring the code for testing purposes.

--- src/types/notes.ts
+++ src/types/notes.ts
@@ -1,6 +1,21 @@
-export type Note = {
-    id: number,
-    title: string,
-    content: string,
-    user: string
+export class Note {
+    id: number;
+    title: string;
+    content: string;
+    user: string;
+
+    constructor(id: number, title: string, content: string, user: string) {
+      this.id = id;
+      this.title = title;
+      this.content = content;
+      this.user = user;
+    }
   }
+
+/**
+ * Represents the raw data structure of notes data, containing an array of notes,
+ * to extract the notes from the raw JSON object.
+ */
+export type NotesRaw = {
+  notes: Note[]
+}
